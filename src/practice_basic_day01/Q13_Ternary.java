a24577f5150754c8492c3cb66ce937d458a1def4
serefahmet21 <serefahmet21@gmail.com>
2022-03-02 00:27:38 +0300

    02.03.22

---
+++ src/practice_basic_day01/Q13_Ternary.java
@@ -0,0 +1,12 @@
+package practice_basic_day01;
+
+public class Q13_Ternary {
+    /*
+     * Ternary kullanarak bir kod yazin. fiyat isminde bir variable tanimlayin. eger fiyat
+     * 10 dan az ise "ucuz" , 10 ile 20 arasinda ise "normal" , 20 ve 20 den buyuk ise pahali
+     * olsun
+     */
+    public static void main(String[] args) {
+
+    }
+}
