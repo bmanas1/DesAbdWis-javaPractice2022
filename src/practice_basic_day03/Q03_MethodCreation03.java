d4d0c8a80a5834e709b46fcedb9e8fa80862cf49
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-03-18 16:43:52 +0300

    18.03.22

--- src/practice_basic_day03/Q03_MethodCreation03.java
+++ src/practice_basic_day03/Q03_MethodCreation03.java
@@ -6,5 +6,21 @@ public class Q03_MethodCreation03 {
     /*
     cm olarak verilen bir sayiyi, metre ve kilometreye donusturen bir method yaziniz
      */
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the value to convert meters and kilometers :");
+        double sentiMeter = scan.nextDouble();
+
+        convertSM(sentiMeter);
+    }
+
+    public static void convertSM(double num) {
+        double meter = num / 100;
+        double kMeter = num / 100000;
+        System.out.println("Your " + num + " sentimeter " + "is equal to " + meter + " Meters " + "and "
+                + kMeter + " kilometers");
+
+    }
 
 }
