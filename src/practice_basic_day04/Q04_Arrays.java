16c29ad1c8a59fc97b1f4e0b5b654c85f1a3faac
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-03-25 21:07:58 +0300

    25.03.22

--- src/practice_basic_day04/Q04_Arrays.java
+++ src/practice_basic_day04/Q04_Arrays.java
@@ -16,7 +16,7 @@ public class Q04_Arrays {
 
     private static void tpMethod(int[] arr, int istenenToplam) {
         for (int i = 0; i < arr.length; i++) { //arr kontrol
-            for (int j = i + 1; j < arr.length; j++) { //2. sayi aliyoruz
+            for (int j = i+1 ; j < arr.length; j++) { //2. sayi aliyoruz  burada j=0 olursa consolde tersini de yazar, j=i olursa da ayni ciktiyi verir
                 if (arr[i] + arr[j] == istenenToplam) {
                     System.out.println(arr[i] + " ve " + arr[j] + " toplami :" + istenenToplam);
                 }
@@ -27,8 +27,19 @@ public class Q04_Arrays {
 
 }
 			/*
+			* j=i+1 ve j=i olursa
 			5 and 4 sum is 9
 			7 and 2 sum is 9
 			-6 and 15 sum is 9
 			8 and 1 sum is 9
+
+			* j=0 olursa tersini de verir
+			5 ve 4 toplami :9
+            7 ve 2 toplami :9
+            -6 ve 15 toplami :9
+            4 ve 5 toplami :9
+            2 ve 7 toplami :9
+            15 ve -6 toplami :9
+            8 ve 1 toplami :9
+            1 ve 8 toplami :9
 			 */
