5a30722149373272bf2d8cc5db94ae37fa4c7a90
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-03-28 13:29:52 +0300

    28.03.22

--- src/practice_basic_day05/Constructor_ThisKeyword.java
+++ src/practice_basic_day05/Constructor_ThisKeyword.java
@@ -2,6 +2,18 @@ package practice_basic_day05;
 
 public class Constructor_ThisKeyword {
     /*
+	 1) Constructor'lar main method'un disinda, class'in icinde olusturulur
+	 2) Constructor'in ismi class ismi ile ayni olmalidir
+	 3) Method ile Constructor Farki: a) Method ismi class isminden farkli olabilir
+	                                     fakat Constructor ismi class ismi ile ayni olmalidir.
+	                                  b) Method'larda return type olmak zorundadir ama
+	                                     Constructor'larda return type olmaz. Constructor'a return
+	                                     type eklerseniz artik o constructor degil method'dur.
+	 4) Bir class'da birden fazla constructor olabilir. Ama constructor'larin parametreleri farkli olmalidir.
+	    Yani; farkli constructor uretmek icin overloading kurallarini kullanmaliyiz.
+	 */
+
+    /*
      * Write program as a Book class that will have 2 Constructors. While creating
      * an object make sure: Instance variables are being initialized Both
      * Constructors are being executed
