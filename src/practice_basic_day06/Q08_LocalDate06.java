69b38d0d1a7b7a787e7669b546a869bec9b07274
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-04-01 22:58:50 +0300

    01.04.22

--- src/practice_basic_day06/Q08_LocalDate06.java
+++ src/practice_basic_day06/Q08_LocalDate06.java
@@ -9,13 +9,6 @@ public class Q08_LocalDate06 {
     // Degisen tarih uzerinden 3 gun, 2 ay ve 5 yil eksilterek son tarihi yaziniz..
 
     public static void main(String[] args) {
-        LocalDate bugun = LocalDate.now();
-        System.out.println("bugun = " + bugun);
 
-        LocalDate degisenTarih = bugun.plusDays(1).plusMonths(1).plusYears(1);
-        System.out.println("degisenTarih = " + degisenTarih);
-
-        LocalDate sonTarih = degisenTarih.minusDays(3).minusMonths(2).minusYears(5);
-        System.out.println("sonTarih = " + sonTarih);
     }
 }
